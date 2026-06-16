8bacdf9052a5c1c88681b339fdeb1c8960851e06
abidsarkar <abidsarkar0@gmail.com>
2026-06-16 22:23:33 +0600

    power square root

--- Aggregation/Weather_Observation_Station_19.sql
+++ Aggregation/Weather_Observation_Station_19.sql
@@ -1,5 +1,8 @@
 -- HackerRank SQL Solution
 -- Problem: Weather Observation Station 19
 -- Category: Aggregation
-
+-- https://www.hackerrank.com/challenges/weather-observation-station-19/problem?isFullScreen=true
 -- Write your solution here
+SELECT 
+ROUND(SQRT(POWER(MAX(LAT_N) - MIN(LAT_N), 2) + POWER(MAX(LONG_W) - MIN(LONG_W), 2)), 4) 
+FROM STATION;
--- Aggregation/Weather_Observation_Station_20.sql
+++ Aggregation/Weather_Observation_Station_20.sql
@@ -1,5 +1,17 @@
 -- HackerRank SQL Solution
 -- Problem: Weather Observation Station 20
 -- Category: Aggregation
-
+-- https://www.hackerrank.com/challenges/weather-observation-station-20/problem?isFullScreen=true
 -- Write your solution here
+SELECT
+Round (S.LAT_N,4)
+from station S
+where (
+    select count(LAT_N)
+    from station
+    where LAT_N<S.LAT_N
+) = (
+    select count(LAT_N)
+    from station
+    where LAT_N> S.LAT_N
+)
