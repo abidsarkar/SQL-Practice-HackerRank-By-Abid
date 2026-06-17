29b6f4101de45ed5f7df6dfac8895a77a6b04b46
abidsarkar <abidsarkar0@gmail.com>
2026-06-17 22:49:00 +0600

    group by and limit

--- Aggregation/Top_Earners.sql
+++ Aggregation/Top_Earners.sql
@@ -1,5 +1,10 @@
 -- HackerRank SQL Solution
 -- Problem: Top Earners
 -- Category: Aggregation
-
+-- https://www.hackerrank.com/challenges/earnings-of-employees/problem?isFullScreen=true
 -- Write your solution here
+SELECT (months * salary) AS total_earnings, COUNT(*)
+FROM Employee
+GROUP BY total_earnings
+ORDER BY total_earnings DESC
+LIMIT 1;
