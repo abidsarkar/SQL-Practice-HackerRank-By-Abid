01b447ef77c417d4bad6fa37a0ee876d59299d8c
abidsarkar <abidsarkar0@gmail.com>
2026-06-21 21:40:45 +0600

    inner query

--- Aggregation/Weather_Observation_Station_15.sql
+++ Aggregation/Weather_Observation_Station_15.sql
@@ -1,5 +1,13 @@
 -- HackerRank SQL Solution
 -- Problem: Weather Observation Station 15
 -- Category: Aggregation
-
+-- https://www.hackerrank.com/challenges/weather-observation-station-15/problem?isFullScreen=true
 -- Write your solution here
+SELECT
+round(LONG_W,4)
+from station
+where LAT_N = (
+    select
+    max(LAT_N)
+    from station
+    where LAT_N<137.2345)
