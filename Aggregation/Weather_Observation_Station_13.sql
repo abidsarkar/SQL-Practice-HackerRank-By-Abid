9768b9a6bab4cf0889102c53bc384e43724d8a7c
abidsarkar <abidsarkar0@gmail.com>
2026-06-21 21:29:29 +0600

    between

--- Aggregation/Weather_Observation_Station_13.sql
+++ Aggregation/Weather_Observation_Station_13.sql
@@ -1,5 +1,9 @@
 -- HackerRank SQL Solution
 -- Problem: Weather Observation Station 13
 -- Category: Aggregation
-
+-- https://www.hackerrank.com/challenges/weather-observation-station-13/problem?isFullScreen=true
 -- Write your solution here
+SELECT
+round(sum(LAT_N),4)
+from station
+where LAT_N between 38.7880 and 137.2345
