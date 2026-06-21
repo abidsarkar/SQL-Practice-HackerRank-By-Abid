29db2cce9437abc173b68d46494a806d6baad412
abidsarkar <abidsarkar0@gmail.com>
2026-06-21 21:24:52 +0600

    second highest

--- Aggregation/Weather_Observation_Station_14.sql
+++ Aggregation/Weather_Observation_Station_14.sql
@@ -1,5 +1,9 @@
 -- HackerRank SQL Solution
 -- Problem: Weather Observation Station 14
 -- Category: Aggregation
-
+--https://www.hackerrank.com/challenges/weather-observation-station-14/problem?isFullScreen=true
 -- Write your solution here
+SELECT
+round(max(LAT_N),4)
+from station
+where LAT_N < 137.2345
