6990d62a24eb822d146bc3fa6aa49c39f4e67295
abidsarkar <abidsarkar0@gmail.com>
2026-06-21 21:44:43 +0600

    min

--- Aggregation/Weather_Observation_Station_17.sql
+++ Aggregation/Weather_Observation_Station_17.sql
@@ -1,5 +1,14 @@
 -- HackerRank SQL Solution
 -- Problem: Weather Observation Station 17
 -- Category: Aggregation
-
+-- https://www.hackerrank.com/challenges/weather-observation-station-17/problem?isFullScreen=true
 -- Write your solution here
+SELECT
+round(LONG_W,4)
+from station
+where LAT_N = (
+    select
+    min(LAT_N)
+    from station
+    where LAT_N > 38.7780
+)
