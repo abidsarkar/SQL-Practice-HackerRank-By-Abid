3f0cb8b933cd8ef9c00901375263681d96b22b01
abidsarkar <abidsarkar0@gmail.com>
2026-06-21 21:20:56 +0600

    basic

--- Aggregation/Weather_Observation_Station_2.sql
+++ Aggregation/Weather_Observation_Station_2.sql
@@ -1,5 +1,9 @@
 -- HackerRank SQL Solution
 -- Problem: Weather Observation Station 2
 -- Category: Aggregation
-
+-- https://www.hackerrank.com/challenges/weather-observation-station-2/problem?isFullScreen=true
 -- Write your solution here
+SELECT
+round(sum(LAT_N),2) as lat  ,
+round(sum(LONG_W),2) as lon 
+from station
